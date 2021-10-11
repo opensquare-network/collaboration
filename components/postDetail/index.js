7d632da53f31251d23614076d871909831d373cd
qiyisi <qiyisi@qq.com>
2021-10-11 14:50:19 +0800

    Fix responsive

--- components/postDetail/index.js
+++ components/postDetail/index.js
@@ -4,7 +4,6 @@ import PostContent from "./postContent";
 import PostTab from "./postTab";
 import PostInfo from "./postInfo";
 import PostResults from "./postResults";
-import { useNetwork } from "utils/hooks";
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,6 +18,9 @@ const MainWrapper = styled.div`
   flex: 1 1 auto;
   > :not(:first-child) {
     margin-top: 20px;
+  }
+  @media screen and (max-width: 800px) {
+    width: 100%;
   }
 `;
 
