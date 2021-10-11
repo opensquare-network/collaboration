807c7ec87f6e0a3da629387d7d4d2a2ef6077ea3
qiyisi <qiyisi@qq.com>
2021-10-11 14:53:48 +0800

    Add no comments

--- components/postDetail/postDiscussion.js
+++ components/postDetail/postDiscussion.js
@@ -71,6 +71,17 @@ const Square = styled.div`
   }
 `;
 
+const NoCommentWrapper = styled.div`
+  display: flex;
+  height: 104px;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 24px;
+  color: #c0c8d5;
+  border-bottom: 1px solid #f0f3f8;
+`;
+
 export default function PostDiscussion({ data, network, comments }) {
   const [content, setContent] = useState("");
   const viewfunc = useViewfunc();
@@ -154,6 +165,9 @@ export default function PostDiscussion({ data, network, comments }) {
           </ContentWrapper>
         </Item>
       ))}
+      {(!comments?.items || comments.items.length === 0) && (
+        <NoCommentWrapper>No current comments</NoCommentWrapper>
+      )}
       <PaginationWrapper>
         <Pagination
           page={comments?.page}
