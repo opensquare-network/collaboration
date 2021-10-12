6f7bdbcdb398614ba29af092e2d67e859adf2776
chaojun <chaojun@opensquare.network>
2021-10-12 17:22:34 +0800

    Refs #154, update

--- services/chainApi.js
+++ services/chainApi.js
@@ -84,14 +84,15 @@ export const signMessage = async (text, address) => {
 };
 
 export const signApiData = async (data, address) => {
-  const msg = JSON.stringify({
+  const dataToSign = {
     ...data,
     timestamp: parseInt(Date.now() / 1000),
-  });
+  };
+  const msg = JSON.stringify(dataToSign);
   const signature = await signMessage(msg, address);
 
   return {
-    data,
+    data: dataToSign,
     address,
     signature,
   };
