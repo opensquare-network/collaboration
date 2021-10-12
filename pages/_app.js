ee041fb8869e92a57ea4769c6bbdee5ef86a78ed
qiyisi <qiyisi@qq.com>
2021-10-12 16:47:57 +0800

    Add loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,32 @@
 import Head from "next/head";
 import { Provider } from "react-redux";
+import NProgress from "nprogress";
+import Router from "next/router";
 
+import "nprogress/nprogress.css";
 import { store } from "../store";
 import "semantic-ui-css/semantic.min.css";
 import "../styles/globals.css";
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on(
+  "routeChangeStart",
+  (url, { shallow }) => !shallow && NProgress.start()
+);
+Router.events.on(
+  "routeChangeComplete",
+  (url, { shallow }) => !shallow && NProgress.done()
+);
+Router.events.on(
+  "routeChangeError",
+  (url, { shallow }) => !shallow && NProgress.done()
+);
 
 function MyApp({ Component, pageProps }) {
   return (
