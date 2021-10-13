3afe17d14ae0b2cc00eefa22d4f287bd4b8b3ab6
yoshiyuki <brother-zhou@qq.com>
2021-10-13 09:44:01 +0800

    fix: apply font-weight 600 for Inter css defination

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+
+export default function NotFound({spaces, hottestProposals}) {
+  return <p style={{fontFamily: "Inter", textAlign: "center", fontSize: 20, fontWeight: 100}}>
+    The uri you input doesn't match any content, please check agagin.
+  </p>;
+}
+
