d7e91f0641779160819c65ec7257b41ca098a856
qiyisi <qiyisi@qq.com>
2021-10-22 11:28:17 +0800

    Fix jest utils

--- utils/index.test.js
+++ utils/index.test.js
@@ -1,32 +1,36 @@
-import { addressEllipsis, fromSymbolUnit, isEmpty, timeDuration, toFixedPrecision, toPrecision } from "./index";
+import {
+  addressEllipsis,
+  fromSymbolUnit,
+  isEmpty,
+  timeDuration,
+  toFixedPrecision,
+  toPrecision,
+} from "./index";
 
 test("addressEllipsis", () => {
-  const address = "hahahahahaha"
-  expect(addressEllipsis(address)).toBe(`haha...haha`)
-})
+  const address = "hahahahahaha";
+  expect(addressEllipsis(address)).toBe(`haha...haha`);
+});
 
 test("timeDuration", () => {
-  Date.now = jest.fn(() => (new Date()).getTime())
-  expect(timeDuration((new Date()).getTime()))
-    .toBe('0 sec ago');
-})
+  Date.now = jest.fn(() => new Date().getTime());
+  expect(["0 sec ago", "a few secs ago"]).toContain(
+    timeDuration(new Date().getTime())
+  );
+});
 
 test("toFixedPrecision", () => {
-  expect(toFixedPrecision("3.1415926", 0, 1))
-    .toBe("3.1");
+  expect(toFixedPrecision("3.1415926", 0, 1)).toBe("3.1");
 });
 
 test("toPrecision", () => {
-  expect(toPrecision("314", 2))
-    .toBe("3.14");
+  expect(toPrecision("314", 2)).toBe("3.14");
 });
 
 test("isEmpty", () => {
-  expect(isEmpty(undefined))
-    .toBe(true);
+  expect(isEmpty(undefined)).toBe(true);
 });
 
 test("fromSymbolUnit", () => {
-  expect(fromSymbolUnit(10000000000, 'DOT'))
-    .toBe("1");
+  expect(fromSymbolUnit(10000000000, "DOT")).toBe("1");
 });
