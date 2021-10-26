3dd05e0478a85ee43d572046a51fe528fb42e500
Yongfeng LI <wliyongfeng@gmail.com>
2021-10-26 11:27:09 +0800

    Add FIXME comment

--- backend/features/accounts/account.controller.js
+++ backend/features/accounts/account.controller.js
@@ -9,6 +9,8 @@ async function getSpaceAccountBalance(ctx) {
   const spaceService = spaceServices[space];
   const api = await spaceService.getApi();
   const blockHeight = snapshot ? parseInt(snapshot) : getLatestHeight(space);
+  // FIXME: 1. check snapshot value, like 'abc'
+  // FIXME: 2. maybe we should check the address format?
   const totalBalance = await getTotalBalanceByHeight(api, blockHeight, address)
 
   ctx.body = {
