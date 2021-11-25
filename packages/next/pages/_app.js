06dee4265298a50edb3b8f3ffa1d86ae0e3158fc
yoshiyuki <brother-zhou@qq.com>
2021-11-25 10:03:07 +0800

    refs #351

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
-        <title>Opensquare - voting</title>
+        <title>OpenSquare Network | Off-chain Voting</title>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
       </Head>
       <Component {...pageProps} />
