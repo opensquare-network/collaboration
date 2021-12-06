22c4237844c32d0c188beace21757a88823aef92
qiyisi <qiyisi@qq.com>
2021-12-06 14:28:47 +0800

    Fix ui, #374

--- packages/next/components/postDetail/postInfo.js
+++ packages/next/components/postDetail/postInfo.js
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
   box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
     0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
   > :not(:first-child) {
-    margin-top: 32px;
+    margin-top: 20px;
   }
   @media screen and (max-width: 800px) {
     padding: 20px;
