be3e701e8ebc84b8c8662ca936979d6e426fce10
yoshiyuki <brother-zhou@qq.com>
2021-12-16 15:02:07 +0800

    SEO: customize space desc

--- packages/next/pages/space/[space]/index.js
+++ packages/next/pages/space/[space]/index.js
@@ -70,7 +70,7 @@ export default function List({
     });
   }
 
-  const desc = `Space for ${spaceName} off-chain voting. You can create, view, and vote proposals. Join off-chain governance of ${spaceName}!`
+  const desc = `Space for ${spaceName} off-chain voting. You can create, view, and vote proposals. Join ${spaceName} off-chain governance!`
 
   return (
     <>
