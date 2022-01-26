3df3863b8e5e7217f3beea63f53cfcfc1dfd135b
hyifeng <chaojun@opensquare.network>
2022-01-26 14:44:34 +0800

    Refs #428, default show all spaces and remember last setting in cookie (#431)

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -3,7 +3,7 @@ import Home from "components/home";
 import { ssrNextApi } from "services/nextApi";
 import { NextSeo } from "next-seo";
 
-export default function Index({ spaces, hottestProposals }) {
+export default function Index({ spaces, hottestProposals, showAllSpace }) {
   const desc = `One of the governance products powered by OpenSquare. It supports relay chains, para chains and assets on Statemine/Statemint, gas free and voting strategies customizable.`;
   return (
     <>
@@ -29,8 +29,7 @@ export default function Index({ spaces, hottestProposals }) {
         }}
       />
       <Layout bgHeight="183px">
-        {/* <Test /> */}
-        <Home spaces={spaces} hottestProposals={hottestProposals} />
+        <Home spaces={spaces} hottestProposals={hottestProposals} showAllSpace={showAllSpace} />
       </Layout>
     </>
   );
@@ -42,10 +41,13 @@ export async function getServerSideProps(context) {
     ssrNextApi.fetch("home/hottest"),
   ]);
 
+  const showAllSpace = context.req.cookies.showallspace;
+
   return {
     props: {
       spaces: spaces || {},
       hottestProposals: hottestProposals || [],
+      showAllSpace: showAllSpace ?? "1",
     },
   };
 }
