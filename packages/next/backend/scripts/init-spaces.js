37d9db81123bf6cb74715a89de85cea97be3b67e
chaojun <chaojun@opensquare.network>
2022-01-26 13:57:55 +0800

    Refs #428, update next to add bifrost support

--- packages/next/backend/scripts/init-spaces.js
+++ packages/next/backend/scripts/init-spaces.js
@@ -75,6 +75,18 @@ const spaces = [
     weightStrategy: ["balance-of","quadratic-balance-of"],
     identity: "kusama",
   },
+  {
+    id: "bifrost",
+    name: "Bifrost",
+    network: "bifrost",
+    symbol: "BNC",
+    ss58Format: 6,
+    decimals: 12,
+    proposeThreshold: "1000000000000",
+    voteThreshold: "10000000000",
+    weightStrategy: ["balance-of","quadratic-balance-of"],
+    identity: "kusama",
+  }
 ];
 
 async function main() {
