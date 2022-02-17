b3e5d31a273fe2df974946922793b543ff48f6dc
yoshiyuki <brother-zhou@qq.com>
2022-02-17 17:23:09 +0800

    improve: snake naming => camel, add doc for <ValueDisplay/>

--- packages/next/components/valueDisplay.js
+++ packages/next/components/valueDisplay.js
@@ -1,14 +1,21 @@
 import Tooltip from "components/tooltip";
 import { abbreviateBigNumber, getEffectiveNumbers, toPrecision } from "frontedUtils";
 
+/**
+ * Render raw data into readable crypto amount
+ * @param {string | BigNumber} value - raw full precision amount
+ * @param {object} space - configuration info for the crypto/chain
+ * @param {boolean} showAEM - Initial of showAlmostEqualMark, default false
+ */
+
 export default function ValueDisplay({ value, space, showAEM = false }) {
-  const lost_precision = (getEffectiveNumbers(value) !== getEffectiveNumbers(abbreviateBigNumber(value)));
+  const lostPrecision = (getEffectiveNumbers(value) !== getEffectiveNumbers(abbreviateBigNumber(value)));
   const precision = toPrecision(value, space?.decimals);
 
-  if (Number(precision) > 1000 || lost_precision) {
+  if (Number(precision) > 1000 || lostPrecision) {
     return (
       <Tooltip size="fit" content={`${precision} ${space?.symbol}`}>
-        <div>{showAEM && lost_precision && "≈"} {`${abbreviateBigNumber(precision)} ${space?.symbol}`}</div>
+        <div>{showAEM && lostPrecision && "≈"} {`${abbreviateBigNumber(precision)} ${space?.symbol}`}</div>
       </Tooltip>
     );
   }
