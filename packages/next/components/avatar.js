546d90fbcfb9c567bdc7231838608a847e74d39c
yoshiyuki <brother-zhou@qq.com>
2022-02-21 15:17:31 +0800

    format codes

--- packages/next/components/avatar.js
+++ packages/next/components/avatar.js
@@ -1,6 +1,6 @@
-import  Identicon  from "@osn/react-identicon";
+import Identicon from "@osn/react-identicon";
 
-export default function Avatar({ address, size = 24 }) {
+export default function Avatar({address, size = 24}) {
   const theme = "polkadot";
-  return <Identicon value={address} size={size} theme={theme} />;
+  return <Identicon value={address} size={size} theme={theme}/>;
 }
