64b87217ab71aa3a3c7b11f1a000f29ff23af481
chaojun <chaojun@opensquare.network>
2022-02-23 12:15:18 +0800

    Remove deplicated api

--- packages/next/backend/features/spaces/routes.js
+++ packages/next/backend/features/spaces/routes.js
@@ -3,7 +3,6 @@ const spaceController = require("./space.controller");
 
 const router = new Router();
 
-router.get("/spaces/:space/networkheights", spaceController.getNetworkHeights);
 router.get("/spaces/:space", spaceController.getSpace);
 router.get("/spaces", spaceController.getSpaces);
 
