a5279f976f31edda49efd863bdff72345b28818b
chaojun <chaojun@opensquare.network>
2022-02-25 11:56:36 +0800

    fix: account login/logout

--- packages/next/frontedUtils/cookie.js
+++ packages/next/frontedUtils/cookie.js
@@ -2,7 +2,7 @@ export function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires;
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 export function getCookie(cname) {
