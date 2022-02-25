b9e6ed1377bc5f1778363cd78257d06aea28a9fc
yoshiyuki <brother-zhou@qq.com>
2022-02-25 16:38:08 +0800

    fix: account seletor for create page

--- packages/next/pages/space/[space]/create.js
+++ packages/next/pages/space/[space]/create.js
@@ -3,8 +3,23 @@ import Nav from "components/nav";
 import PostCreate from "@/components/postCreate";
 import { ssrNextApi } from "services/nextApi";
 import { to404 } from "../../../frontedUtils/serverSideUtil";
+import { setAvailableNetworks } from "../../../store/reducers/accountSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import pick from "lodash/pick";
 
 export default function Create({ space }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      setAvailableNetworks(
+        space?.networks?.map((item) => pick(item, ["network", "ss58Format"])) ||
+          []
+      )
+    );
+  }, [dispatch, space]);
+
   return (
     <Layout bgHeight="183px" space={space}>
       <Nav
@@ -22,9 +37,7 @@ export default function Create({ space }) {
 export async function getServerSideProps(context) {
   const { space: spaceId } = context.params;
 
-  const [
-    { result: space },
-  ] = await Promise.all([
+  const [{ result: space }] = await Promise.all([
     ssrNextApi.fetch(`spaces/${spaceId}`),
   ]);
 
