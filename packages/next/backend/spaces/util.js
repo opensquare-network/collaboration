4c2dd05b6ac22ea3b2914258bfe0e2efe032accf
hyifeng <chaojun@opensquare.network>
2022-03-01 16:22:15 +0800

    Fix: identity (#554)
    * Fix: identity
    
    * Add polkadot identity

--- packages/next/backend/spaces/util.js
+++ packages/next/backend/spaces/util.js
@@ -1,42 +1,9 @@
-const { getApi, getTotalBalance, getTokenBalance } = require("../services/node.service");
 const { getSpaceCollection } = require("../mongo");
-
-function getNetworkIdentity(network) {
-  switch (network) {
-    case "kusama":
-      return {
-        network: "kusama",
-        ss58Format: 2,
-      };
-    default:
-      return undefined;
-  }
-}
-
-function createSpace(spaceConfig) {
-  const { symbol, networks } = spaceConfig;
-
-  return {
-    ...spaceConfig,
-    // A asset can be appear on multiple networks,
-    // Each network has its own identity
-    networks: networks?.map(network => {
-      const { identity } = network;
-      return {
-        ...network,
-        identity: getNetworkIdentity(identity),
-      };
-    }),
-  };
-}
 
 async function getSpaces() {
   const spaceCol = await getSpaceCollection();
   const spaceConfigs = await spaceCol.find({}).toArray();
-  return spaceConfigs.map(spaceConfig => {
-    const space = createSpace(spaceConfig);
-    return space;
-  });
+  return spaceConfigs;
 }
 
 module.exports = {
--- packages/next/services/util.js
+++ packages/next/services/util.js
@@ -1,3 +1,23 @@
+function getNetworkIdentity(network) {
+  switch (network) {
+    case "polkadot": {
+      return {
+        network: "polkadot",
+        ss58Format: 0,
+      };
+    }
+    case "kusama": {
+      return {
+        network: "kusama",
+        ss58Format: 2,
+      };
+    }
+    default: {
+      return undefined;
+    }
+  }
+}
+
 export const findNetworkConfig = (space, network) => {
   const networkConfig = space?.networks?.find(item => item.network === network);
   if (!networkConfig) {
@@ -8,5 +28,6 @@ export const findNetworkConfig = (space, network) => {
     ...networkConfig,
     symbol: space?.symbol,
     decimals: space?.decimals,
+    identity: getNetworkIdentity(networkConfig.identity),
   }
 };
