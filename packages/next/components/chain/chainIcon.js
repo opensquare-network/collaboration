7d54edc443690ecc3bba59e31d0fdcc8777de5ab
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-01 12:35:20 +0800

    Add missed chain icon shown on heights

--- packages/next/components/chain/chainIcon.js
+++ packages/next/components/chain/chainIcon.js
@@ -3,9 +3,17 @@ import Bifrost from "public/imgs/icons/chain/bifrost.svg";
 import Polkadot from "public/imgs/icons/chain/polkadot.svg";
 import Karura from "public/imgs/icons/chain/karura.svg";
 import Khala from "public/imgs/icons/chain/khala.svg";
+import Kusama from "public/imgs/icons/chain/kusama.svg";
+import Kintsugi from "public/imgs/logos/kintsugi.svg"
 import Default from "public/imgs/icons/chain/default.svg";
 
 function ChainIcon({ chainName }) {
+  if (chainName === "kintsugi") {
+    return <Kintsugi />
+  }
+  if (chainName === "kusama") {
+    return <Kusama />
+  }
   if (chainName === "statemine") {
     return <Statemine />;
   }
