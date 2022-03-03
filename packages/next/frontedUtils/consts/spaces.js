d25ba9888db5d4d7b38c2665a0ab7ffc48276f83
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-03 10:19:13 +0800

    Use Seo component on home page

--- packages/next/frontedUtils/consts/spaces.js
+++ packages/next/frontedUtils/consts/spaces.js
@@ -6,9 +6,9 @@ export const SPACES = Object.freeze({
   kusama: "kusama",
   polarisdao: "polarisdao",
   polkadot: "polkadot",
-  ['rmrk-curation']: "rmrk-curation",
+  ["rmrk-curation"]: "rmrk-curation",
   rmrk: "rmrk",
-})
+});
 
 export const spaceToSeoImageMap = Object.freeze({
   bifrost: "bafybeigefryb4kw7wskzv7cc6jtbj5454vmsyzlddeuowg4fsd2lnui37m",
@@ -18,6 +18,10 @@ export const spaceToSeoImageMap = Object.freeze({
   kusama: "bafybeidiohh5piq2dt5y2u3oqg4dtw3l3ok7yp5a5pmr73zs7mzsf6lbcm",
   polarisdao: "bafybeib3tf3iv22iydsmg534uva2okh7xawoyspf5oqmbzrjova46ttvuq",
   polkadot: "bafybeihaby2kn5w6663ftuihg4rlx7cdmwwus745gbxl7c26uyxkcm4mq4",
-  ['rmrk-curation']: "bafybeic42ekrlmnjdwfi3kdshqfuvo76yhugu5nbkewebnigjxxe5hqjty",
+  ["rmrk-curation"]:
+    "bafybeic42ekrlmnjdwfi3kdshqfuvo76yhugu5nbkewebnigjxxe5hqjty",
   rmrk: "bafybeibhjq4ls327o52xm76zgabalkltmdofzku57sfjw2afsao3ud2yjq",
-})
+});
+
+export const defaultSeoImage =
+  "bafybeibosq47gohmbpqevt4utw5ooi3dkf4k2qky6lq2qp2bvf6cq3k52y";
--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -1,35 +1,19 @@
 import Layout from "components/layout";
 import Home from "components/home";
 import { ssrNextApi } from "services/nextApi";
-import { NextSeo } from "next-seo";
+import Seo from "@/components/seo";
 
 export default function Index({ spaces, hottestProposals, showAllSpace }) {
   const desc = `One of the governance products powered by OpenSquare. It supports relay chains, para chains and assets on Statemine/Statemint, gas free and voting strategies customizable.`;
   return (
     <>
-      <NextSeo
-        title="OpenSquare Off-chain Voting"
-        description={desc}
-        openGraph={{
-          url: "https://www.opensquare.io/",
-          title: "OpenSquare Off-chain Voting",
-          description: desc,
-          images: [
-            {
-              url: "https://test.opensquare.io/imgs/logo.jpg",
-              width: 1200,
-              height: 628,
-            },
-          ],
-        }}
-        twitter={{
-          handle: "@handle",
-          site: "@site",
-          cardType: "summary_large_image",
-        }}
-      />
+      <Seo desc={desc} />
       <Layout bgHeight="183px">
-        <Home spaces={spaces} hottestProposals={hottestProposals} showAllSpace={showAllSpace} />
+        <Home
+          spaces={spaces}
+          hottestProposals={hottestProposals}
+          showAllSpace={showAllSpace}
+        />
       </Layout>
     </>
   );
