3a7a4ca7c82f96c3ddd52430c392cff26a0885f1
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-26 22:49:37 +0800

    Add interlay identity chain map

--- packages/next/frontedUtils/consts/identity.js
+++ packages/next/frontedUtils/consts/identity.js
@@ -4,4 +4,5 @@ export const identityChainMap = Object.freeze({
   [CHAINS.kintsugi]: [CHAINS.kusama],
   [CHAINS.statemine]: [CHAINS.kusama],
   [CHAINS.karura]: [CHAINS.kusama],
-})
+  [CHAINS.interlay]: [CHAINS.polkadot],
+});
