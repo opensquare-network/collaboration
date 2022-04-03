62c951f1235de8570160546c8bf880dc8cf8a5b3
Yongfeng LI <wliyongfeng@gmail.com>
2022-04-03 15:05:29 +0800

    Update README

--- packages/node-api/src/features/proxy/proxy.controller.js
+++ packages/node-api/src/features/proxy/proxy.controller.js
@@ -1,40 +1,43 @@
+import { evmChains } from "@collaboration/voting/frontedUtils/consts/chains";
+
 const { getBlockApi } = require("../utils");
 const { getApis } = require("../../apis");
-const { u8aToHex } = require("@polkadot/util")
+const { u8aToHex } = require("@polkadot/util");
 const { decodeAddress } = require("@polkadot/util-crypto");
 const { chains } = require("../../constants");
 
 function addrToPubkey(address) {
-  return u8aToHex(decodeAddress(address))
+  return u8aToHex(decodeAddress(address));
 }
 
-async function getProxyFromOneApi(api, delegator, toCheckDelegate, blockHashOrHeight) {
+async function getProxyFromOneApi(
+  api,
+  delegator,
+  toCheckDelegate,
+  blockHashOrHeight
+) {
   let blockApi = await getBlockApi(api, blockHashOrHeight);
   const data = await blockApi.query.proxy.proxies(delegator);
   const [proxies] = data.toJSON() || [];
 
   return (proxies || []).some(({ delegate: itemDelegate, proxyType }) => {
-      const delegateKey = addrToPubkey(itemDelegate);
-      const toCheckKey = addrToPubkey(toCheckDelegate);
+    const delegateKey = addrToPubkey(itemDelegate);
+    const toCheckKey = addrToPubkey(toCheckDelegate);
 
-      if (delegateKey !== toCheckKey) {
-        return false
-      }
-
-      return [
-        "Any",
-        "NonTransfer",
-        "Governance",
-        "Assets",
-      ].includes(proxyType)
+    if (delegateKey !== toCheckKey) {
+      return false;
     }
-  )
+
+    return ["Any", "NonTransfer", "Governance", "Assets"].includes(proxyType);
+  });
 }
 
 async function getProxyFromApis(apis, delegator, delegatee, blockHashOrHeight) {
   const promises = [];
   for (const api of apis) {
-    promises.push(getProxyFromOneApi(api, delegator, delegatee, blockHashOrHeight));
+    promises.push(
+      getProxyFromOneApi(api, delegator, delegatee, blockHashOrHeight)
+    );
   }
 
   return Promise.any(promises);
@@ -44,31 +47,36 @@ class ProxyController {
   async getProxyRelationship(ctx) {
     const { chain, delegator, delegatee, blockHashOrHeight } = ctx.params;
     if (!delegator) {
-      ctx.throw(400, "No delegator given")
-      return
+      ctx.throw(400, "No delegator given");
+      return;
     }
 
     if (!delegatee) {
-      ctx.throw(400, "No delegatee given")
+      ctx.throw(400, "No delegatee given");
     }
 
-    if (chains.kintsugi === chain) {
-      ctx.body = { isProxy: false }
-      return
+    if ([chains.kintsugi, chains.interlay, ...evmChains].includes(chain)) {
+      ctx.body = { isProxy: false };
+      return;
     }
 
     const apis = getApis(chain);
-    if (apis.every(api => !api.isConnected)) {
-      ctx.throw(500, "No apis connected")
-      return
+    if (apis.every((api) => !api.isConnected)) {
+      ctx.throw(500, "No apis connected");
+      return;
     }
 
     try {
-      const isProxy = await getProxyFromApis(apis, delegator, delegatee, blockHashOrHeight);
-      ctx.body = { isProxy }
+      const isProxy = await getProxyFromApis(
+        apis,
+        delegator,
+        delegatee,
+        blockHashOrHeight
+      );
+      ctx.body = { isProxy };
     } catch (e) {
-      console.error('Get proxy from node fail', e)
-      ctx.throw(500, "Failed to query proxy from node")
+      console.error("Get proxy from node fail", e);
+      ctx.throw(500, "Failed to query proxy from node");
     }
   }
 }
