5c7652cd50f0cc86dd2b3502d0952c5be393eb0a
2nthony <ant.hu@outlook.com>
2022-04-13 10:41:57 +0800

    fix(next): one line voting display (#675)
    * feat(next): biased voting item component
    
    * refactor(next): postResult use biasedVotingItem
    
    * fix(next): biasedVotingItem tooltip
    
    * chore: typo
    
    * refactor: prefer camel-case const name
    
    * fix(next): typo
    
    * fix(next): clean
    
    * fix(next): css
    
    * refactor(next): remove default value
    
    * chore(next): use span as label
    
    * fix: inline <span> won't ellipsis
    
    Co-authored-by: yoshiyuki <brother-zhou@qq.com>

---
+++ packages/next/components/postDetail/biasedVotingItem.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import Tooltip from "../tooltip";
+import ValueDisplay from "../valueDisplay";
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const LabelWrapper = styled.div`
+  color: #506176;
+  position: relative;
+  display: flex;
+  width: 60%;
+
+  // for tooltip with size=fit
+  > *:first-child {
+    max-width: 100%;
+  }
+`;
+
+const Label = styled.span`
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const ValueDisplayWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 40%;
+`;
+
+function BiasedVotingItem({ label, value, space }) {
+  return (
+    <Wrapper>
+      <LabelWrapper>
+        <Tooltip content={label} size="fit">
+          <Label>{label}</Label>
+        </Tooltip>
+      </LabelWrapper>
+
+      <ValueDisplayWrapper>
+        <ValueDisplay value={value} space={space} />
+      </ValueDisplayWrapper>
+    </Wrapper>
+  );
+}
+
+export default BiasedVotingItem;
