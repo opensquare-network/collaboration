74c9081bac75b902f4120f58570be80bf4601ed4
2nthony <ant.hu@outlook.com>
2022-04-14 16:12:56 +0800

    feat(next): Optimize the space information display (#677)
    * feat(next): listInfo ellipsis the strategies
    
    * fix(next): sort
    
    * fix(next): listInfo clean strategy css
    
    * fix(next): listInfo
    
    * feat(next): listInfo network
    
    * fix(next): listInfo
    
    remove tooltip
    
    * fix(next): listInfo
    
    underline for title
    
    * feat(next): listInfo abbreviation threshold
    
    * feat(next): listInfo abbreviation threshold
    
    * feat(next): universal component modal
    
    semantic ui react
    
    * feat(next): listInfo more info
    
    * feat(next): listInfo show info by clicking about title
    
    * refactor(next): listInfo rename const
    
    * refactor(next): listInfo move
    
    * feat(next): listInfo info modal add close button
    
    * refactor(next): listInfo extract details as a component
    
    * chore: clean
    
    * refactor(next): self closed tag
    
    * fix(next): listInfo use span tag and add title prop
    
    * fix(next): listInfo/details set logoName line-height 28px
    
    * fix(next): details detailsLabel line height
    
    * refactor(next): details move
    
    * fix(next): details color
    
    * fix(next): details icon
    
    * fix(next): details use abbreviateBigNumber for threshold
    
    * feat(next): details use ValueDisplay

---
+++ packages/next/components/listInfo/details.js
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+import {
+  p_14_normal,
+  p_14_medium,
+  p_16_semibold,
+  p_20_semibold,
+} from "../../styles/textStyles";
+import SpaceLogo from "@/components/spaceLogo";
+import ChainIcon from "../chain/chainIcon";
+import Divider from "../styled/divider";
+import { capitalize } from "frontedUtils";
+import ValueDisplay from "../valueDisplay";
+
+const Wrapper = styled.div``;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  // override 'SpaceLogo' margin-right
+  > :first-child {
+    margin-right: 0;
+  }
+`;
+
+const LogoName = styled.div`
+  ${p_20_semibold};
+  margin-top: 16px;
+  text-transform: capitalize;
+  line-height: 28px;
+`;
+
+const LogoSymbol = styled.div`
+  ${p_14_normal};
+  color: #a1a8b3;
+`;
+
+const DetailsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DetailsTitle = styled.div`
+  ${p_16_semibold};
+`;
+
+const DetailsItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 16px;
+  line-height: 24px;
+`;
+
+const DetailsLabel = styled.span`
+  ${p_14_medium};
+  color: #506176;
+`;
+
+const DetailsValue = styled.div`
+  ${p_14_medium};
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const DetailsNetwork = styled.span`
+  margin-right: 8px;
+`;
+
+export default function Details({ space }) {
+  const strategyCount = space.weightStrategy?.length || 0;
+  const networkCount = space.networks?.length || 0;
+
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <SpaceLogo spaceId={space.id} />
+        <LogoName>{space.name}</LogoName>
+        <LogoSymbol>{space.symbol}</LogoSymbol>
+      </LogoWrapper>
+
+      <DetailsWrapper>
+        <DetailsTitle>About</DetailsTitle>
+        <img src="/imgs/icons/info.svg" />
+      </DetailsWrapper>
+
+      <Divider />
+
+      <div>
+        <DetailsItem>
+          <DetailsLabel>Threshold</DetailsLabel>
+          <DetailsValue>
+            <ValueDisplay value={space.proposeThreshold} space={space} />
+          </DetailsValue>
+        </DetailsItem>
+
+        <DetailsItem>
+          <DetailsLabel>Strategies({strategyCount})</DetailsLabel>
+          <div>
+            {space.weightStrategy?.map((strategy, index) => (
+              <DetailsValue key={index}>{strategy}</DetailsValue>
+            ))}
+          </div>
+        </DetailsItem>
+
+        <DetailsItem>
+          <DetailsLabel>Networks({networkCount})</DetailsLabel>
+          <div>
+            {space.networks?.map((network, index) => (
+              <DetailsValue key={index}>
+                <DetailsNetwork>{capitalize(network.network)}</DetailsNetwork>{" "}
+                <ChainIcon chainName={network.network} size={20} />
+              </DetailsValue>
+            ))}
+          </div>
+        </DetailsItem>
+      </div>
+    </Wrapper>
+  );
+}
---
+++ packages/next/components/listInfo/index.js
@@ -0,0 +1,184 @@
+import styled from "styled-components";
+import {
+  p_14_normal,
+  p_16_semibold,
+  p_20_semibold,
+} from "../../styles/textStyles";
+import SpaceLogo from "@/components/spaceLogo";
+import ChainIcon from "../chain/chainIcon";
+import ValueDisplay from "../valueDisplay";
+import Modal from "../modal";
+import { useState } from "react";
+import Details from "./details";
+import Button from "../button";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media screen and (max-width: 900px) {
+    display: none;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoName = styled.div`
+  ${p_20_semibold};
+  text-transform: capitalize;
+`;
+
+const LogoSymbol = styled.div`
+  ${p_14_normal};
+  color: #a1a8b3;
+`;
+
+const AboutWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const AboutItem = styled.div`
+  display: flex;
+  align-items: flex-start;
+  position: relative;
+`;
+
+const AboutIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 8px;
+`;
+
+const AboutName = styled.div`
+  ${p_16_semibold};
+  margin-bottom: 2px;
+  cursor: pointer;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const AboutDetail = styled.div`
+  font-size: 14px;
+  line-height: 24px;
+  color: #a1a8b3;
+  text-transform: capitalize;
+`;
+
+const StrategyAboutDetail = styled.span`
+  font-size: 14px;
+  line-height: 24px;
+  color: #a1a8b3;
+  max-width: 240px;
+`;
+
+const AboutDivider = styled.div`
+  width: 1px;
+  height: 16px;
+  background: #e2e8f0;
+  margin: 0 40px;
+`;
+
+const ChainIconsWrapper = styled.div`
+  font-size: 14px;
+  line-height: 24px;
+  color: #a1a8b3;
+  display: flex;
+`;
+
+const ChainIcons = styled.div`
+  display: flex;
+  svg,
+  img {
+    margin-right: 4px;
+    vertical-align: middle;
+  }
+`;
+
+const ModalActions = styled.div`
+  margin-top: 20px;
+  text-align: right;
+`;
+
+const ModalCloseButton = styled(Button)`
+  display: inline-block;
+`;
+
+export default function ListInfo({ space }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const strategyCount = space.weightStrategy?.length || 0;
+  const networkCount = space.networks?.length || 0;
+
+  const handleShowModal = () => {
+    setModalOpen(true);
+  };
+
+  return (
+    <Wrapper>
+      <LogoWrapper>
+        <SpaceLogo spaceId={space.id} />
+        <div>
+          <LogoName>{space.name}</LogoName>
+          <LogoSymbol>{space.symbol}</LogoSymbol>
+        </div>
+      </LogoWrapper>
+      <AboutWrapper>
+        <AboutItem>
+          <AboutIcon src="/imgs/icons/threshold.svg" />
+          <div>
+            <AboutName onClick={handleShowModal}>Threshold</AboutName>
+            <AboutDetail>
+              <ValueDisplay value={space.proposeThreshold} space={space} />
+            </AboutDetail>
+          </div>
+        </AboutItem>
+        <AboutDivider />
+        <AboutItem>
+          <AboutIcon src="/imgs/icons/strategy.svg" />
+          <div>
+            <AboutName onClick={handleShowModal}>
+              Strategy({strategyCount})
+            </AboutName>
+            <StrategyAboutDetail title={space.weightStrategy?.join(", ")}>
+              {space.weightStrategy?.[0]}
+              {space.weightStrategy?.length > 1 && ", ..."}
+            </StrategyAboutDetail>
+          </div>
+        </AboutItem>
+        <AboutDivider />
+        <AboutItem>
+          <AboutIcon src="/imgs/icons/network.svg" />
+          <div>
+            <AboutName onClick={handleShowModal}>
+              Network({networkCount})
+            </AboutName>
+            <ChainIconsWrapper>
+              <ChainIcons>
+                {space.networks?.slice(0, 3).map((network, index) => (
+                  <ChainIcon key={index} chainName={network.network} />
+                ))}
+              </ChainIcons>
+              {space.networks?.length > 3 && "..."}
+            </ChainIconsWrapper>
+          </div>
+        </AboutItem>
+
+        <Modal open={modalOpen} setOpen={setModalOpen}>
+          <Details space={space} />
+
+          <ModalActions>
+            <ModalCloseButton onClick={() => setModalOpen(false)}>
+              Close
+            </ModalCloseButton>
+          </ModalActions>
+        </Modal>
+      </AboutWrapper>
+    </Wrapper>
+  );
+}
