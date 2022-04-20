e9743cff9427b1dfb68b6a2b867f0d2075361e8d
2nthony <ant.hu@outlook.com>
2022-04-20 09:59:40 +0800

    build(next): svg usage

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -2,8 +2,17 @@ module.exports = {
   webpack(config) {
     config.module.rules.push(
       {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          "@svgr/webpack",
+          {
+            loader: "file-loader",
+            options: {
+              name: "static/media/[name].[hash].[ext]",
+            },
+          },
+        ],
       },
       {
         test: /\/common\/.*\.js/,
