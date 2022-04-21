455984a21363fcf6bd20752a11e04e928e537ef6
yoshiyuki <brother-zhou@qq.com>
2022-04-21 14:43:48 +0800

    Style polish (#701)
    * style：ellipsis text display voter name, #612
    
    * implement ui: voter bio popup card , #612
    
    * implement ui: show tooltip for vote choice, #612

--- packages/next/components/role/voter.js
+++ packages/next/components/role/voter.js
@@ -9,6 +9,8 @@ import IdentityIcon from "@/components/identityIcon";
 import styled from "styled-components";
 import ChainIcon from "@/components/chain/chainIcon";
 import { evmChains } from "../../frontedUtils/consts/chains";
+import Popup from "@/components/popup";
+import Flex from "@/components/styled/flex";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,10 +34,41 @@ const Name = styled.span`
 const IdentityWrapper = styled.span`
   display: inline-flex;
   align-items: center;
+  max-width: calc(100% - 10px);
 
   > :not(:first-child) {
     margin-left: 4px;
   }
+  > span:last-child {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+`;
+
+const TextMinor = styled.span`
+  color: #506176;
+  word-break: break-all;
+`;
+
+const PopupCard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 288px;
+  color: #1e2134;
+  > div:first-child {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const Divider = styled.div`
+  margin-top: 12px;
+  margin-bottom: 12px;
+  height: 1px;
+  width: 100%;
+  background-color: #f0f3f8;
 `;
 
 export default function Voter({ address, network, showNetwork = true }) {
@@ -61,24 +94,38 @@ export default function Voter({ address, network, showNetwork = true }) {
       .catch(() => {});
   }, [network, address, isMounted, isEvm]);
 
+  const popup = (
+    <PopupCard>
+      <div>
+        <Avatar address={address} size={20} />
+        <ChainIcon chainName={network} size={16} />
+        <Name>{addressEllipsis(address)}</Name>
+      </div>
+      <Divider />
+      <TextMinor>{address}</TextMinor>
+    </PopupCard>
+  );
+
   return (
     <Wrapper>
       <Avatar address={address} size={20} />
       {showNetwork && <ChainIcon chainName={network} size={16} />}
-      <ExternalLink href={link}>
-        {identity?.info && identity?.info?.status !== "NO_ID" ? (
-          <IdentityWrapper>
-            <IdentityIcon
-              status={identity.info.status}
-              showTooltip
-              size={showNetwork ? 12 : 14}
-            />
-            <Name>{identity.info.display}</Name>
-          </IdentityWrapper>
-        ) : (
-          <Name>{addressEllipsis(address)}</Name>
-        )}
-      </ExternalLink>
+      <Popup content={popup}>
+        <ExternalLink href={link}>
+          {identity?.info && identity?.info?.status !== "NO_ID" ? (
+            <IdentityWrapper>
+              <IdentityIcon
+                status={identity.info.status}
+                showTooltip
+                size={showNetwork ? 12 : 14}
+              />
+              <Name title={identity.info.display}>{identity.info.display}</Name>
+            </IdentityWrapper>
+          ) : (
+            <Name>{addressEllipsis(address)}</Name>
+          )}
+        </ExternalLink>
+      </Popup>
     </Wrapper>
   );
 }
