dec83a27058f92fabab40d8d2aceca800a504900
2nthony <ant.hu@outlook.com>
2022-05-18 11:52:07 +0800

    fix(next): fix identity icon size

--- packages/next/components/identityOrAddr.js
+++ packages/next/components/identityOrAddr.js
@@ -9,17 +9,17 @@ const IdentityWrapper = styled.span`
   > span:first-child {
     height: 24px;
   }
-  
+
   > :not(:first-child) {
     margin-left: 4px;
   }
 
   ${(p) =>
-          p.ellipsis &&
-          css`
-            > span:last-child {
-              word-break: break-all;
-            }
+    p.ellipsis &&
+    css`
+      > span:last-child {
+        word-break: break-all;
+      }
     `}
 `;
 
@@ -29,14 +29,10 @@ const Name = styled.span`
   line-height: 24px;
 `;
 
-export default function IdentityOrAddr({identity, addr, showNetwork = false,ellipsis = false }) {
+export default function IdentityOrAddr({ identity, addr, ellipsis = false }) {
   return identity?.info && identity?.info?.status !== "NO_ID" ? (
     <IdentityWrapper ellipsis={ellipsis}>
-      <IdentityIcon
-        status={identity.info.status}
-        showTooltip
-        size={showNetwork ? 12 : 14}
-      />
+      <IdentityIcon status={identity.info.status} showTooltip size={12} />
       <Name title={identity.info.display}>{identity.info.display}</Name>
     </IdentityWrapper>
   ) : (
