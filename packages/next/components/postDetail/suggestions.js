f18c5cc97083f6f69aa60d31598f886f77878fb7
2nthony <ant.hu@outlook.com>
2022-07-05 10:25:52 +0800

    feat(next): mention (#845)
    * feat(next): mention
    
    * feat: mention item render as author/networkuser

---
+++ packages/next/components/postDetail/suggestions.js
@@ -0,0 +1,68 @@
+import { addressEllipsis } from "frontedUtils";
+import { identityChainMap } from "frontedUtils/consts/identity";
+import { uniqWith } from "lodash";
+import { useCallback, useEffect, useState } from "react";
+import { fetchIdentity } from "services/identity";
+import Author from "../author";
+
+export function useSuggestions(comments = []) {
+  const [suggestions, setSuggestions] = useState();
+
+  const resolveMentionFormat = (identity, user) =>
+    `[@${identity?.info?.display || addressEllipsis(user.address)}](${
+      user.address
+    }-${user.network}) `;
+
+  const fetchIdentitySuggestions = useCallback(async () => {
+    const userIdentities = await Promise.all(
+      uniqWith(
+        (comments?.items || []).map((comment) => {
+          return {
+            address: comment.address,
+            network: comment.commenterNetwork,
+          };
+        }),
+        (a, b) => a.address === b.address && a.network === b.network
+      ).map(async (item) => {
+        const identityChain = identityChainMap[item.network] || item.network;
+        const identity = await fetchIdentity(identityChain, item.address);
+        return {
+          ...item,
+          identity,
+        };
+      })
+    );
+
+    return userIdentities.map((user) => {
+      return {
+        address: user.address,
+        value: resolveMentionFormat(user.identity, user),
+        preview: (
+          <Author
+            showNetwork
+            address={user.address}
+            space={{ network: user.network }}
+            noLink
+          />
+        ),
+      };
+    });
+  }, [comments]);
+
+  const loadSuggestions = (text) => {
+    return suggestions.filter((i) =>
+      i.address.toLowerCase().includes(text.toLowerCase())
+    );
+  };
+
+  useEffect(() => {
+    fetchIdentitySuggestions().then((v) => {
+      setSuggestions(v);
+    });
+  }, [comments, fetchIdentitySuggestions]);
+
+  return {
+    suggestions,
+    loadSuggestions,
+  };
+}
