d5b4104433c7d1dbd6f36d0967e8af67c0fa04bf
Yongfeng LI <wliyongfeng@gmail.com>
2022-08-24 15:13:45 +0800

    Support altair on backend, #884

---
+++ backend/packages/backend/src/scripts/spaces/altair.js
@@ -0,0 +1,22 @@
+const { networks, strategies } = require("./consts");
+
+const altairConfig = {
+  id: networks.altair,
+  name: "Altair",
+  symbol: "AIR",
+  decimals: 18,
+  networks: [
+    {
+      network: networks.centrifuge,
+      ss58Format: 136,
+    },
+  ],
+  proposeThreshold: "1000000000000000000",
+  voteThreshold: "1000000000000000000",
+  weightStrategy: [strategies.balanceOf, strategies.quadraticBalanceOf],
+  version: "2",
+};
+
+module.exports = {
+  altairConfig,
+};
