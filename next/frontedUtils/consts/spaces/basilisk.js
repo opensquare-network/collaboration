92540019bc0a28d787288f9ac7d3a660d5f7765a
Yongfeng LI <wliyongfeng@gmail.com>
2022-11-06 22:25:07 +0800

    Support basilisk and hydradx in next package

---
+++ next/frontedUtils/consts/spaces/basilisk.js
@@ -0,0 +1,7 @@
+const basilisk = {
+  spaceIcon: "basilisk.svg",
+  seoImage: "bafybeihuz5wbvovfq7a4xzii4ynytvwq6cwhbzaprqye6bsc26hnlia6tu",
+  fromIcon: "project-basilisk.svg",
+};
+
+export default basilisk;
