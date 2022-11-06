1b7b36ddf853ba2e67bc1aafb11fdd224df77d24
Yongfeng LI <wliyongfeng@gmail.com>
2022-11-06 21:10:38 +0800

    Support basilisk/hydradx on backend side

---
+++ backend/packages/backend/src/scripts/spaces/basilisk.js
@@ -0,0 +1,22 @@
+const { networks, strategies } = require("./consts");
+
+const basiliskConfig = {
+  id: "basilisk",
+  name: "Basilisk",
+  symbol: "BSX",
+  decimals: 12,
+  networks: [
+    {
+      network: networks.basilisk,
+      ss58Format: 10041,
+    },
+  ],
+  proposeThreshold: "1000000000000",
+  voteThreshold: "10000000000",
+  weightStrategy: [strategies.balanceOf, strategies.quadraticBalanceOf],
+  version: "3",
+};
+
+module.exports = {
+  basiliskConfig,
+};
