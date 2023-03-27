d15a7287125b1b1e6c1daaaa03c7bddc7a5bd0bb
chaojun <chaojun@opensquare.network>
2023-03-27 11:12:48 +0800

    Fix

--- next/hooks/notification/useNotifications.js
+++ next/hooks/notification/useNotifications.js
@@ -2,12 +2,15 @@ import { EmptyQuery } from "frontedUtils/constants";
 import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { loginAddressSelector } from "store/reducers/accountSlice";
+import { unreadSelector } from "store/reducers/notificationSlice";
 import nextApi from "../../services/nextApi";
 
 export function useNotifications({ page, pageSize }) {
   const [notifications, setNotifications] = useState(EmptyQuery);
   const [isLoading, setIsLoading] = useState(true);
   const address = useSelector(loginAddressSelector);
+  const unread = useSelector(unreadSelector);
+  const [prevUnread, setPrevUnread] = useState(unread);
 
   const refresh = useCallback(() => {
     if (!address) {
@@ -15,10 +18,7 @@ export function useNotifications({ page, pageSize }) {
     }
 
     nextApi
-      .fetch(
-        `account/${address}/notifications`,
-        { page, pageSize },
-      )
+      .fetch(`account/${address}/notifications`, { page, pageSize })
       .then(({ result }) => {
         if (result) {
           setNotifications(result);
@@ -34,10 +34,7 @@ export function useNotifications({ page, pageSize }) {
     setIsLoading(true);
 
     nextApi
-      .fetch(
-        `account/${address}/notifications`,
-        { page, pageSize },
-      )
+      .fetch(`account/${address}/notifications`, { page, pageSize })
       .then(({ result }) => {
         if (result) {
           setNotifications(result);
@@ -49,6 +46,15 @@ export function useNotifications({ page, pageSize }) {
         setIsLoading(false);
       });
   }, [address, page, pageSize]);
+
+  useEffect(() => {
+    // Got new notifications
+    if (unread > prevUnread) {
+      refresh();
+    }
+
+    setPrevUnread(unread);
+  }, [unread, prevUnread, refresh]);
 
   return { isLoading, notifications, refresh };
 }
