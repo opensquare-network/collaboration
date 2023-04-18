2ee297e1b8171a46eba152a074b253b26667c33c
chaojun <chaojun@opensquare.network>
2023-04-18 10:26:50 +0800

    Remove unused code

--- backend/packages/node-api/src/features/tokenMeta/routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router");
-const { getNativeTokenInfo } = require("./getNativeTokenInfo");
-
-const router = new Router();
-
-router.get("/token/native/metadata", getNativeTokenInfo);
-
-module.exports = router;
