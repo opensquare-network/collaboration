c2d6c8e82209b6c52c30764857a2fd8cf716b116
chaojun <chaojun@opensquare.network>
2023-04-24 16:58:11 +0800

    refactor check code

--- backend/packages/backend/src/features/spaces/createSpace.js
+++ backend/packages/backend/src/features/spaces/createSpace.js
@@ -8,17 +8,75 @@ const { strategies } = require("../../consts/voting");
 const dataUriToBuffer = (dataUri) =>
   import("data-uri-to-buffer").then(({ default: fn }) => fn(dataUri));
 
-async function createSpace(ctx) {
-  const {
-    name,
-    logo,
-    assets,
-    symbol,
-    decimals,
-    proposalThreshold,
-    weightStrategy,
-  } = ctx.request.body;
+function checkAssetParams({
+  chain,
+  decimals,
+  ss58Format,
+  symbol,
+  votingThreshold,
+  votingWeight,
+  assetId,
+  contract,
+  type,
+}) {
+  if (!chain) {
+    throw new HttpError(400, "Asset chain is required");
+  }
 
+  if (ss58Format === undefined) {
+    throw new HttpError(400, "Asset ss58 format is required");
+  }
+
+  if (ss58Format < 0) {
+    throw new HttpError(400, "Asset ss58Format can't be negative");
+  }
+
+  if (!symbol) {
+    throw new HttpError(400, "Asset symbol is required");
+  }
+
+  if (decimals === undefined) {
+    throw new HttpError(400, "Asset decimals is required");
+  }
+
+  if (decimals <= 0) {
+    throw new HttpError(400, "Asset decimals must be greater than 0");
+  }
+
+  if (votingThreshold === undefined) {
+    throw new HttpError(400, "Asset voting threshold is required");
+  }
+
+  if (votingThreshold < 0) {
+    throw new HttpError(400, "Asset voting threshold can't be negative");
+  }
+
+  if (votingWeight === undefined) {
+    throw new HttpError(400, "Asset voting weight is required");
+  }
+
+  if (votingWeight < 0) {
+    throw new HttpError(400, "Asset voting weight can't be negative");
+  }
+
+  if (type === "erc20" && !contract) {
+    throw new HttpError(400, "Asset contract is required");
+  }
+
+  if (type === "asset" && assetId === undefined) {
+    throw new HttpError(400, "Asset id is required");
+  }
+}
+
+function checkSpaceParams({
+  name,
+  logo,
+  assets,
+  symbol,
+  decimals,
+  proposalThreshold,
+  weightStrategy,
+}) {
   if (!name) {
     throw new HttpError(400, "Name is required");
   }
@@ -61,6 +119,24 @@ async function createSpace(ctx) {
     throw new HttpError(400, "Assets are required");
   }
 
+  for (const asset of assets) {
+    checkAssetParams(asset);
+  }
+}
+
+async function createSpace(ctx) {
+  checkSpaceParams(ctx.request.body);
+
+  const {
+    name,
+    logo,
+    assets,
+    symbol,
+    decimals,
+    proposalThreshold,
+    weightStrategy,
+  } = ctx.request.body;
+
   const assetsByNetwork = assets.reduce((acc, asset) => {
     const {
       chain,
@@ -73,46 +149,6 @@ async function createSpace(ctx) {
       contract,
       type,
     } = asset;
-
-    if (!chain) {
-      throw new HttpError(400, "Asset chain is required");
-    }
-
-    if (!symbol) {
-      throw new HttpError(400, "Asset symbol is required");
-    }
-
-    if (decimals === undefined) {
-      throw new HttpError(400, "Asset decimals is required");
-    }
-
-    if (decimals <= 0) {
-      throw new HttpError(400, "Asset decimals must be greater than 0");
-    }
-
-    if (votingThreshold === undefined) {
-      throw new HttpError(400, "Asset voting threshold is required");
-    }
-
-    if (votingThreshold < 0) {
-      throw new HttpError(400, "Asset voting threshold can't be negative");
-    }
-
-    if (votingWeight === undefined) {
-      throw new HttpError(400, "Asset voting weight is required");
-    }
-
-    if (votingWeight < 0) {
-      throw new HttpError(400, "Asset voting weight can't be negative");
-    }
-
-    if (type === "erc20" && !contract) {
-      throw new HttpError(400, "Asset contract is required");
-    }
-
-    if (type === "asset" && assetId === undefined) {
-      throw new HttpError(400, "Asset id is required");
-    }
 
     if (!acc[chain]) {
       acc[chain] = { network: chain, ss58Format, assets: [] };
