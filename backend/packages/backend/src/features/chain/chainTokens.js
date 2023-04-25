35839628ff8e6b47f26c4ee44f4b8e331b1a6ee5
chaojun <chaojun@opensquare.network>
2023-04-25 12:13:57 +0800

    Optimize query

--- backend/packages/backend/src/features/chain/chainTokens.js
+++ backend/packages/backend/src/features/chain/chainTokens.js
@@ -1,11 +1,8 @@
-const omit = require("lodash.omit");
 const { getChainTokenCollection } = require("../../mongo");
 
 async function getTokensDefinition(ctx) {
   const chainTokenCol = await getChainTokenCollection();
-  const tokens = await chainTokenCol.find({}).toArray();
-
-  ctx.body = tokens.map((item) => omit(item, ["_id"]));
+  ctx.body = await chainTokenCol.find({}, { projection: { _id: 0 } }).toArray();
 }
 
 module.exports = {
--- backend/packages/backend/src/features/chain/chains.js
+++ backend/packages/backend/src/features/chain/chains.js
@@ -1,11 +1,8 @@
-const omit = require("lodash.omit");
 const { getChainCollection } = require("../../mongo");
 
 async function getChainsDefinition(ctx) {
   const chainCol = await getChainCollection();
-  const chains = await chainCol.find({}).toArray();
-
-  ctx.body = chains.map((item) => omit(item, ["_id"]));
+  ctx.body = await chainCol.find({}, { projection: { _id: 0 } }).toArray();
 }
 
 module.exports = {
