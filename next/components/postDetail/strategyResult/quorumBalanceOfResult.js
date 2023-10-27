84b32d46e5f01bc7da162fca0862bf390abb0a14
chaojun <chaojun@opensquare.network>
2023-10-27 12:46:31 +0800

    refactor

--- next/components/postDetail/strategyResult/quorumBalanceOfResult.js
+++ next/components/postDetail/strategyResult/quorumBalanceOfResult.js
@@ -1,19 +1,8 @@
-import ValueDisplay from "@/components/valueDisplay";
 import BalanceOfResult from "./balanceOfResult";
-import {
-  Divider,
-  FlexAround,
-  OptionIndex,
-  ProgressItem,
-  ResultHead,
-  ResultName,
-  StatusItem,
-  StatusWrapper,
-} from "./common/styled";
 import BigNumber from "bignumber.js";
+import Quorum from "./common/quorum";
 
 export default function QuorumBalanceOfResult({ proposal, space, voteStatus }) {
-  const isEnded = new Date().getTime() > proposal?.endDate;
   const votedBalance = proposal?.votedWeights?.balanceOf || 0;
 
   const firstChoice = proposal?.choices?.[0];
@@ -30,6 +19,7 @@ export default function QuorumBalanceOfResult({ proposal, space, voteStatus }) {
   const isFirstChoiceWin = gteFirstChoiceVoteBalanceCount < 1;
   const isQuorumReached = new BigNumber(votedBalance).gte(space?.quorum);
   const isPass = isQuorumReached && isFirstChoiceWin;
+  const isEnded = new Date().getTime() > proposal?.endDate;
 
   return (
     <>
@@ -38,36 +28,12 @@ export default function QuorumBalanceOfResult({ proposal, space, voteStatus }) {
         space={space}
         voteStatus={voteStatus}
       />
-      <ResultHead>
-        <ResultName>Quorum</ResultName>
-      </ResultHead>
-      <Divider />
-      <div>
-        <ProgressItem>
-          <OptionIndex>Quorum</OptionIndex>
-          <FlexAround>
-            <ValueDisplay value={votedBalance} space={space} noSymbol />
-            <span>&nbsp;/&nbsp;</span>
-            <ValueDisplay
-              value={space?.quorum || 0}
-              space={space}
-              noSymbol
-              className="quorum-value"
-            />
-          </FlexAround>
-        </ProgressItem>
-      </div>
-      <StatusWrapper>
-        <StatusItem positive={isPass}>
-          {isPass
-            ? isEnded
-              ? "Passed"
-              : "Passing"
-            : isEnded
-            ? "Failed"
-            : "Failing"}
-        </StatusItem>
-      </StatusWrapper>
+      <Quorum
+        votedBalance={votedBalance}
+        space={space}
+        isPass={isPass}
+        isEnded={isEnded}
+      />
     </>
   );
 }
--- next/components/postDetail/strategyResult/quorumQuadraticBalanceOfResult.js
+++ next/components/postDetail/strategyResult/quorumQuadraticBalanceOfResult.js
@@ -1,15 +1,5 @@
-import ValueDisplay from "@/components/valueDisplay";
+import Quorum from "./common/quorum";
 import QuadraticBalanceOfResult from "./quadraticBalanceOfResult";
-import {
-  Divider,
-  FlexAround,
-  OptionIndex,
-  ProgressItem,
-  ResultHead,
-  ResultName,
-  StatusItem,
-  StatusWrapper,
-} from "./common/styled";
 import BigNumber from "bignumber.js";
 
 export default function QuorumQuadraticBalanceOfResult({
@@ -17,7 +7,6 @@ export default function QuorumQuadraticBalanceOfResult({
   space,
   voteStatus,
 }) {
-  const isEnded = new Date().getTime() > proposal?.endDate;
   const votedBalance = proposal?.votedWeights?.balanceOf || 0;
 
   const firstChoice = proposal?.choices?.[0];
@@ -34,6 +23,7 @@ export default function QuorumQuadraticBalanceOfResult({
   const isFirstChoiceWin = gteFirstChoiceVoteBalanceCount < 1;
   const isQuorumReached = new BigNumber(votedBalance).gte(space?.quorum);
   const isPass = isQuorumReached && isFirstChoiceWin;
+  const isEnded = new Date().getTime() > proposal?.endDate;
 
   return (
     <>
@@ -42,36 +32,12 @@ export default function QuorumQuadraticBalanceOfResult({
         space={space}
         voteStatus={voteStatus}
       />
-      <ResultHead>
-        <ResultName>Quorum</ResultName>
-      </ResultHead>
-      <Divider />
-      <div>
-        <ProgressItem>
-          <OptionIndex>Quorum</OptionIndex>
-          <FlexAround>
-            <ValueDisplay value={votedBalance} space={space} noSymbol />
-            <span>&nbsp;/&nbsp;</span>
-            <ValueDisplay
-              value={space?.quorum || 0}
-              space={space}
-              noSymbol
-              className="quorum-value"
-            />
-          </FlexAround>
-        </ProgressItem>
-      </div>
-      <StatusWrapper>
-        <StatusItem positive={isPass}>
-          {isPass
-            ? isEnded
-              ? "Passed"
-              : "Passing"
-            : isEnded
-            ? "Failed"
-            : "Failing"}
-        </StatusItem>
-      </StatusWrapper>
+      <Quorum
+        votedBalance={votedBalance}
+        space={space}
+        isPass={isPass}
+        isEnded={isEnded}
+      />
     </>
   );
 }
