2b2b448074bc42c5584b4e502971ea592e5a7c0b
2nthony <ant.hu@outlook.com>
2023-10-31 16:39:30 +0800

    Revert "build: fix"
    This reverts commit 0b3d0537d41bc21abcd8eb1cd4c9509bda387059.

--- next/next.config.js
+++ next/next.config.js
@@ -1,17 +1,42 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@osn/common-ui", "@osn/common", "@osn/rich-text-editor"],
-  compiler: {
-    styledComponents: {
-      ssr: true,
-    },
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+// https://www.reddit.com/r/reactjs/comments/qz21jg/nextjs_how_to_load_svg_that_is_required_in/
+const withTM = require("next-transpile-modules")([
+  "@osn/common-ui",
+  "@osn/common",
+  "@osn/rich-text-editor",
+]);
 
+module.exports = withTM({
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          {
+            loader: "file-loader",
+            options: {
+              name: "static/media/[name].[hash].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\/common\/.*\.js/,
+        use: "babel-loader",
+      }
+    );
     return config;
   },
-};
+});
