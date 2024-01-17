089c0558f1d2bb81fe5c37c9af54f617327dc0e1
2nthony <ant.hu@outlook.com>
2024-01-17 16:09:58 +0800

    replace 04d2c5 text color

--- next/components/newSpace/step2/newAssetButton/index.js
+++ next/components/newSpace/step2/newAssetButton/index.js
@@ -13,7 +13,7 @@ const Text = styled.span`
   font-weight: 600;
   font-size: 16px;
   line-height: 24px;
-  color: #04d2c5;
+  var(--textBrandSecondary);
 `;
 
 export default function NewAssetButton({ onClick = noop }) {
