50696dae90396d92008a3304f04ef56ce1fcf1e8
2nthony <ant.hu@outlook.com>
2024-01-18 15:11:50 +0800

    fix: flash of invisible text (FOIT)

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -10,6 +10,8 @@ import "../styles/globals.css";
 import "react-mde/lib/styles/css/react-mde-all.css";
 import "@osn/common-ui/styles/index.css";
 import "@osn/previewer/styles.css";
+import { cn } from "@osn/common-ui";
+import { Inter, Montserrat } from "next/font/google";
 
 NProgress.configure({
   minimum: 0.3,
@@ -31,15 +33,47 @@ Router.events.on(
   (url, { shallow }) => !shallow && NProgress.done(),
 );
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Head>
-        <title>OpenSquare Network Off-chain Voting</title>
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
-      </Head>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <style jsx global>{`
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+          --font-montserrat: ${montserrat.style.fontFamily};
+        }
+        html,
+        body {
+          font-family: ${inter.style.fontFamily} -apple-system, "Helvetica Neue",
+            Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC",
+            "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+        }
+      `}</style>
+
+      <Provider store={store}>
+        <Head>
+          <title>OpenSquare Network Off-chain Voting</title>
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no"
+          />
+        </Head>
+        <main
+          className={cn(inter.className, inter.variable, montserrat.variable)}
+        >
+          <Component {...pageProps} />
+        </main>
+      </Provider>
+    </>
   );
 }
 
