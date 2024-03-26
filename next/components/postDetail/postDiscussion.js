21375c22b8670b3a75c4140a5b8bb1d2065480d6
2nthony <ant.hu@outlook.com>
2024-03-26 16:55:23 +0800

    lint

--- next/components/postDetail/postDiscussion.js
+++ next/components/postDetail/postDiscussion.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
