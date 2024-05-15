0d5f8ba0da0709bfc6703a5375ef8a5bf15e324f
chaojun <chaojun@opensquare.network>
2024-05-15 12:38:01 +0800

    fix: template page mobile layout, #1024

--- next/components/postSettings/index.js
+++ next/components/postSettings/index.js
@@ -19,7 +19,7 @@ export default function PostSettings({ space, settings }) {
   }, [dispatch, space]);
 
   return (
-    <div className="flex mt-[20px] gap-[20px]">
+    <div className="flex mt-[20px] gap-[20px] max-sm:flex-col max-sm:mx-[-20px]">
       <SettingsNavigation />
       <ProposalTemplate space={space} settings={settings} />
     </div>
