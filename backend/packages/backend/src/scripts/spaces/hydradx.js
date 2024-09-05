9a42f10aeaaf8aeb58f508e23d4c226df83963ac
Chaojun Huang <chaojun@opensquare.network>
2024-09-05 11:47:34 +0800

    Create hydration space, #1036 (#1038)
    * Update hydration assets, #1036
    
    * Update chain icon, #1036
    
    * Revert "Update hydration assets, #1036"
    
    This reverts commit fe0eb23611877b3d35557e76d62707663b99e2af.
    
    * fix: hydration as new space, #1036

--- backend/packages/backend/src/scripts/spaces/hydradx.js
+++ backend/packages/backend/src/scripts/spaces/hydradx.js
@@ -1,25 +1,25 @@
 const { Accessibility } = require("../../consts/space");
 const { networks, strategies } = require("./consts");
 
+const hydradxNetwork = {
+  network: networks.hydradx,
+  ss58Format: 63,
+  assets: [
+    {
+      symbol: "HDX",
+      decimals: 12,
+      votingThreshold: "10000000000",
+    },
+  ],
+};
+
 const hydradxConfig = {
   id: "hydradx",
   name: "HydraDX",
   symbol: "HDX",
   decimals: 12,
   accessibility: Accessibility.PUBLIC,
-  networks: [
-    {
-      network: networks.hydradx,
-      ss58Format: 63,
-      assets: [
-        {
-          symbol: "HDX",
-          decimals: 12,
-          votingThreshold: "10000000000",
-        },
-      ],
-    },
-  ],
+  networks: [hydradxNetwork],
   proposeThreshold: "1000000000000",
   weightStrategy: [strategies.balanceOf, strategies.quadraticBalanceOf],
   version: "4",
@@ -28,6 +28,20 @@ const hydradxConfig = {
   admins: [],
 };
 
+const hydrationConfig = {
+  ...hydradxConfig,
+  id: "hydration",
+  name: "Hydration",
+  networks: [
+    {
+      ...hydradxNetwork,
+      network: networks.hydration,
+    },
+  ],
+  spaceIcon: "hydration.svg",
+};
+
 module.exports = {
   hydradxConfig,
+  hydrationConfig,
 };
