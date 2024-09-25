03c3772c92ceaa7f3125cc4f9d46e6e58fb8801e
chaojun <chaojun@opensquare.network>
2024-09-25 16:20:36 +0800

    Simplify, #1051

--- next/components/chainIcon.js
+++ next/components/chainIcon.js
@@ -1,23 +1,13 @@
 import camalCase from "lodash.camelcase";
 import { ChainIcon as _ChainIcon, Tooltip } from "@osn/common-ui";
-import { networks } from "frontedUtils/consts/chains/networks";
 import { normalizeChainName } from "frontedUtils/chain";
 
-function getIconChainName(chainName) {
-  if (networks.bifrost === chainName) {
-    return networks.bifrostKusama;
-  }
-  return chainName;
-}
-
 export function ChainIcon({ chainName, size }) {
+  const normalizedChainName = normalizeChainName(chainName);
   return (
-    <Tooltip content={normalizeChainName(chainName)}>
+    <Tooltip content={normalizedChainName}>
       <div>
-        <_ChainIcon
-          chainName={camalCase(getIconChainName(chainName))}
-          size={size}
-        />
+        <_ChainIcon chainName={camalCase(normalizedChainName)} size={size} />
       </div>
     </Tooltip>
   );
