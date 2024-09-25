a1f6a32e74a64d4333eeb9121393b94f2105b255
chaojun <chaojun@opensquare.network>
2024-09-25 14:32:27 +0800

    Update, #1051

--- backend/packages/backend/src/scripts/spaces/bifrostPolkadot.js
+++ backend/packages/backend/src/scripts/spaces/bifrostPolkadot.js
@@ -19,6 +19,30 @@ const bifrostPolkadotConfig = {
         },
       ],
     },
+    {
+      network: networks.karura,
+      ss58Format: 8,
+      assets: [
+        {
+          type: "token",
+          symbol: "BNC",
+          decimals: 12,
+          votingThreshold: "10000000000",
+        },
+      ],
+    },
+    {
+      network: networks.moonriver,
+      assets: [
+        {
+          type: "erc20",
+          contract: "0xfffffffff075423be54811ecb478e911f22dde7d",
+          symbol: "BNC",
+          decimals: 12,
+          votingThreshold: "10000000000",
+        },
+      ],
+    },
   ],
   proposeThreshold: "1000000000000",
   weightStrategy: [strategies.balanceOf, strategies.quadraticBalanceOf],
