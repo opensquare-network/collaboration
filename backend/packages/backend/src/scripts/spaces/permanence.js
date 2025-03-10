45d5e56cef05185fd512aec435557b5e52de7be6
Yongfeng LI <wliyongfeng@gmail.com>
2025-03-10 11:24:39 +0800

    Update whitelist addresses for permanence dao

--- backend/packages/backend/src/scripts/spaces/permanence.js
+++ backend/packages/backend/src/scripts/spaces/permanence.js
@@ -14,12 +14,13 @@ const config = {
     "12His7t3EJ38tjdBbivUzWQeaNCLKfMqtKp1Ed3xHMyCE9N3", // The Ionian Group
     "12s6UMSSfE2bNxtYrJc6eeuZ7UxQnRpUzaAh1gPQrGNFnE8h", // Polkadotters
     "13EDmaUe89xXocPppFmuoAZaCsckaJy3deAyVyiykk1zKQbF", // PMEI
-    "14333MZvbGkcq5CZ8fYHZiFYwHNDaW3uiErDKMb7oqnupWXn", // Transistor
+    // "14333MZvbGkcq5CZ8fYHZiFYwHNDaW3uiErDKMb7oqnupWXn", // Transistor
     "14gMJV95zwxUsFEZDSC8mtBVifS6SypKJkfBKANkMsLZdeVb", // Yongfeng Li
     "14Gn7SEmCgMX7Ukuppnw5TRjA7pao2HFpuJo39frB42tYLEh", // EzioRed
     "15fTH34bbKGMUjF1bLmTqxPYgpg481imThwhWcQfCyktyBzL", // Helikon
     "167YoKNriVtP4Nxk9F9GRV7HTKu5VnxaRq1pKMANAnmmTY9F", // José Rabasso
     "13znFMMjHyM2UvSewvaKMC2bLUcySRMzcM8BAMTzm1G2P5ju", // PERMANENCE DAO/GOV-PROXY
+    "12KtA8mtfsK1CyQb4utLiwG3ao22z77w2cM2GqnaL2RiDCoJ", // Flez
   ],
   networks: [
     {
