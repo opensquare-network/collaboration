feb9140bc4c8317695a488f2bb6eee193340c96b
Yongfeng LI <wliyongfeng@gmail.com>
2025-03-31 16:39:24 +0800

    Add Fluffy Labs address to jamdao, #1097

--- backend/packages/backend/src/scripts/spaces/jamdao.js
+++ backend/packages/backend/src/scripts/spaces/jamdao.js
@@ -23,6 +23,7 @@ const config = {
     "123KEnwuHKiu48WsfwAX4YonGFTpkEmWS6V2WGoJA1qzBkbS", // MORUM
     "15p3jWZaP4dHTkDTuKM5VXQL5XGfH9U5r6Ntu3UAv2K7vPb8", // Tessera
     "15R1pWegyu7AfMev8DBMT67qxYoJhA1v7BbA2nn7S2uJ5QDF", // JamBrains
+    "15fGrDWmFoaApnZXEw1Zg45zuCqpkbMS9YPzczeU9dHseUUu", // Fluffy Labs
   ],
   networks: [
     {
