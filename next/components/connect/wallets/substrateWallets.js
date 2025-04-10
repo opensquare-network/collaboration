fdf038e5466cff4870714b9a564410737283696b
chaojun <chaojun@opensquare.network>
2025-04-10 11:25:35 +0800

    Add loading and not installed display, #1059

--- next/components/connect/wallets/substrateWallets.js
+++ next/components/connect/wallets/substrateWallets.js
@@ -3,6 +3,7 @@ import { StyledTitle } from "../styled";
 import { substrateWallets } from "./consts";
 import useInjectedExtension from "./useInjectedExtension";
 import WalletTypes from "./walletTypes";
+import { SystemDownload, SystemLoading1 } from "@osn/icons/opensquare";
 
 function SubstrateWallet({ wallet, onClick }) {
   const extensionName =
@@ -22,21 +23,27 @@ function SubstrateWallet({ wallet, onClick }) {
   return (
     <div
       className={cn(
-        installed ? "cursor-pointer" : "pointer-events-none",
-        !installed && "bg-fillBgTertiary",
+        installed ? "cursor-pointer" : "bg-fillBgTertiary",
         "flex flex-col justify-center items-center gap-[12px] w-[136px] h-[96px]",
         "border border-strokeActionDefault",
         installed && "hover:border-strokeActionActive",
       )}
-      onClick={onClick}
+      onClick={installed ? onClick : undefined}
     >
       <Logo className="w-[32px] h-[32px]" />
-      {installed ? (
+      {loading ? (
+        <div className="flex gap-[4px] text-textTertiary">
+          <label className="text14Semibold">{wallet.title}</label>
+          <SystemLoading1 className="w-[16px] h-[16px] [&_path]:fill-textTertiary" />
+        </div>
+      ) : installed ? (
         <label className="text14Semibold">{wallet.title}</label>
       ) : (
         <div className="flex gap-[4px] text-textTertiary">
           <label className="text14Semibold">{wallet.title}</label>
-          <div className="textBrandSecondary">D</div>
+          <a href={`${wallet.installUrl}`} target="_blank" rel="noreferrer">
+            <SystemDownload className="w-[16px] h-[16px] [&_path]:fill-textBrandSecondary" />
+          </a>
         </div>
       )}
     </div>
