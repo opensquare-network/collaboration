1137200c8115ef570f89ecf4e7142b5c59cb3780
chaojun <chaojun@opensquare.network>
2025-06-24 12:17:22 +0800

    Add space search API, #1158

--- backend/packages/backend/src/features/spaces/space.controller.js
+++ backend/packages/backend/src/features/spaces/space.controller.js
@@ -2,9 +2,16 @@ const { pick } = require("lodash");
 const spaceService = require("../../services/space.service");
 const { getAllSpaces } = require("../../spaces");
 const { extractPage } = require("../../utils");
+const { searchSpaces } = require("./searchSpaces");
 
 async function getSpaces(ctx) {
   const { page, pageSize } = extractPage(ctx);
+  const { search } = ctx.query;
+  if (search) {
+    ctx.body = await searchSpaces(search, page, pageSize);
+    return;
+  }
+
   const allSpaces = await spaceService.getSpaces();
   const items = allSpaces.slice((page - 1) * pageSize, page * pageSize);
   ctx.body = {
