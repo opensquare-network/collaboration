511b5c38b39b33751af7891cfa47da4090af1a2c
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-07-01 14:29:48 +0800

    Clear active prop waring for option component , #1127 (#1179)

--- next/components/option.js
+++ next/components/option.js
@@ -1,24 +1,25 @@
-import { Button, cn } from "@osn/common-ui";
-import styled from "styled-components";
+import { Button } from "@osn/common-ui";
+import styled, { css } from "styled-components";
 
 const OptionButton = styled(Button)`
   height: auto;
-  background-color: var(--fillBgPrimary);
-  border-color: var(--strokeActionDefault);
+  ${(p) =>
+    p.active &&
+    css`
+      border-color: var(--textBrandPrimary);
+      color: var(--textBrandPrimary);
+    `}
+`;
+
+const IndexSpan = styled.span`
+  color: var(--textTertiary);
+  ${(p) => p.active && "color: var(--textBrandPrimary);"}
 `;
 
 function Option({ item, index, active, ...props }) {
   return (
-    <OptionButton block className={active} active={active} {...props}>
-      <span
-        className={cn(
-          "float-left",
-          "text-textTertiary",
-          active && "text-textBrandPrimary",
-        )}
-      >
-        #{index}
-      </span>
+    <OptionButton active={active} block {...props}>
+      <IndexSpan active={active}>#{index}</IndexSpan>
       <div
         title={item}
         className="w-full px-2 overflow-hidden text-ellipsis sm:text-nowrap text-wrap"
