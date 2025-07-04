47c7bbb1b46722672fdce5628b81d4de80974288
quinn-gao <quinnn.gao@gmail.com>
2025-07-04 14:36:06 +0800

    fix: style, #1185

--- next/components/option.js
+++ next/components/option.js
@@ -4,22 +4,25 @@ import styled, { css } from "styled-components";
 const OptionButton = styled(Button)`
   height: auto;
   ${(p) =>
-    p.active &&
+    p.$active &&
     css`
       border-color: var(--textBrandPrimary);
       color: var(--textBrandPrimary);
+      &:hover {
+        border-color: var(--textBrandPrimary);
+      }
     `}
 `;
 
 const IndexSpan = styled.span`
   color: var(--textTertiary);
-  ${(p) => p.active && "color: var(--textBrandPrimary);"}
+  ${(p) => p.$active && "color: var(--textBrandPrimary);"}
 `;
 
 function Option({ item, index, active, ...props }) {
   return (
-    <OptionButton active={active} block {...props}>
-      <IndexSpan active={active}>#{index}</IndexSpan>
+    <OptionButton $active={active} block {...props}>
+      <IndexSpan $active={active}>#{index}</IndexSpan>
       <div
         title={item}
         className="w-full px-2 overflow-hidden text-ellipsis sm:text-nowrap text-wrap"
