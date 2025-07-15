ed79b8e98308fdee55944328066bb97d17a21f9b
chaojun <chaojun@opensquare.network>
2025-07-15 14:35:43 +0800

    Add script to update spaces without modifying admins and members

--- backend/packages/backend/src/scripts/update-spaces.js
+++ backend/packages/backend/src/scripts/update-spaces.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const omit = require("lodash.omit");
 const spaces = require("./spaces");
 const { getSpaceCollection } = require("../mongo");
 
@@ -8,10 +9,9 @@ async function main() {
   const spaceCol = await getSpaceCollection();
   const bulk = spaceCol.initializeUnorderedBulkOp();
   for (const space of spaces) {
-    const spaceData = {
-      offline: false, // set default offline space
-      ...space,
-    };
+    // Omit members and admins from the space data to avoid overwriting them
+    // Because these fields could be modified by API
+    const spaceData = omit(space, ["members", "admins"]);
     bulk.find({ id: space.id }).upsert().update({ $set: spaceData });
   }
   await bulk.execute();
