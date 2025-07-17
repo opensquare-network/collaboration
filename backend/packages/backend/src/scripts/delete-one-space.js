052afff6ec21a1d5e8118bc1c5f4e6492655c358
leochen <18340836866@163.com>
2025-07-17 19:03:45 +0800

    Add a script to delete a space and all its related data, #1195 (#1196)
    * Add a script to delete a space and all its related data, #1195
    
    * refactor, #1195
    
    * refactor, #1195
    
    * Refactor, #1195
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

---
+++ backend/packages/backend/src/scripts/delete-one-space.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+
+const minimist = require("minimist");
+const {
+  getSpaceCollection,
+  getProposalCollection,
+  getVoteCollection,
+  getCommentCollection,
+} = require("../mongo");
+
+function getSpaceId() {
+  const args = minimist(process.argv.slice(2));
+  if (!args.space) {
+    throw new Error("Must specify space id with argument --space=[spaceId]");
+  }
+  return args.space;
+}
+
+async function throwIfNoSpace(spaceId) {
+  const spaceCol = await getSpaceCollection();
+  const space = await spaceCol.findOne({ id: spaceId });
+  if (!space) {
+    throw new Error(`Space with id "${spaceId}" not found.`);
+  }
+}
+
+async function deleteSpace(spaceId) {
+  const spaceCol = await getSpaceCollection();
+  await spaceCol.deleteOne({ id: spaceId });
+}
+
+async function getProposalIds(spaceId) {
+  const proposalCol = await getProposalCollection();
+  const proposals = await proposalCol.find({ space: spaceId }).toArray();
+  const proposalIds = proposals?.map((p) => p._id);
+
+  return proposalIds || [];
+}
+
+async function deleteVotes(proposalIds) {
+  const voteCol = await getVoteCollection();
+  await voteCol.deleteMany({
+    proposal: { $in: proposalIds },
+  });
+}
+
+async function deleteComments(proposalIds) {
+  const commentCol = await getCommentCollection();
+  await commentCol.deleteMany({
+    proposal: { $in: proposalIds },
+  });
+}
+
+async function deleteProposals(spaceId) {
+  const proposalCol = await getProposalCollection();
+  await proposalCol.deleteMany({
+    space: spaceId,
+  });
+}
+
+async function main() {
+  const spaceId = getSpaceId();
+  await throwIfNoSpace(spaceId);
+
+  const proposalIds = await getProposalIds(spaceId);
+  if (proposalIds.length > 0) {
+    await deleteComments(proposalIds);
+    await deleteVotes(proposalIds);
+  }
+  await deleteProposals(spaceId);
+  await deleteSpace(spaceId);
+
+  console.log(`Delete space ${spaceId} and related data successfully`);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => {
+    process.exit();
+  });
