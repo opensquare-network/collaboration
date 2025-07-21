e46743c9f1959a49c21194d3032d9f4a312a5f43
quinn-gao <quinnn.gao@gmail.com>
2025-07-21 15:35:07 +0800

    fix: change editor mention-list style, #1198

--- next/components/editor/index.js
+++ next/components/editor/index.js
@@ -1,26 +1,38 @@
-import { cn } from "@osn/common-ui";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import LoadingEditor from "./loading";
+import styled from "styled-components";
 
 const RichEditor = dynamic(
   () => import("@osn/common-ui").then((mod) => mod.RichEditor),
   { ssr: false, loading: () => <LoadingEditor /> },
 );
 
+const EditorWrapper = styled.div`
+  position: relative;
+  min-height: 182px;
+  @media (max-width: 640px) {
+    min-height: ${({ $preview }) => ($preview ? "182px" : "222px")};
+  }
+
+  & .rich-editor {
+    background-color: var(--fillBgPrimary);
+  }
+
+  div ul.mention-list {
+    background: var(--fillBgPrimary);
+    li[aria-selected="true"],
+    li:hover {
+      background-color: var(--fillBgQuaternary);
+    }
+  }
+`;
+
 export default function Editor(props) {
   const [isPreview, setIsPreview] = useState(false);
   return (
-    <div
-      className={cn(
-        "relative",
-        isPreview
-          ? "min-h-[182px] max-sm:min-h-[182px]"
-          : "min-h-[182px] max-sm:min-h-[222px]",
-        "[&_.rich-editor]:bg-fillBgPrimary",
-      )}
-    >
+    <EditorWrapper $preview={isPreview}>
       <RichEditor {...props} onChangePreviewMode={setIsPreview} />
-    </div>
+    </EditorWrapper>
   );
 }
