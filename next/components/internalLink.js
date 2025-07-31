5f5cfa0ffda447946047980b0b80860d0013713f
quinn-gao <quinnn.gao@gmail.com>
2025-07-31 16:01:50 +0800

    refactor: replace InternalLink component with direct usage of NextLink for improved navigation

--- next/components/internalLink.js
+++ next/components/internalLink.js
@@ -1,5 +1,39 @@
-import Link from "next/link";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+const Wrapper = styled.div`
+  cursor: pointer;
+  > a {
+    :hover {
+      color: inherit;
+      text-decoration: underline;
+    }
+  }
+`;
 
 export default function InternalLink({ href, children }) {
-  return href ? <Link href={href}>{children}</Link> : <>{children}</>;
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    router.push(href);
+  };
+
+  return href ? (
+    <Wrapper
+      onClick={handleClick}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          handleClick(e);
+        }
+      }}
+      role="link"
+      tabIndex={0}
+    >
+      {children}
+    </Wrapper>
+  ) : (
+    <>{children}</>
+  );
 }
