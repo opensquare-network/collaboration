e19e21bd39336c9bc7fc75320ac4671f3f8ec467
chaojun <chaojun@opensquare.network>
2025-08-18 17:51:00 +0800

    fix: get user votes, #1227

--- backend/packages/backend/src/features/proposals/getUserVotesOfProposals.js
+++ backend/packages/backend/src/features/proposals/getUserVotesOfProposals.js
@@ -2,7 +2,6 @@ const { HttpError } = require("../../exc");
 const { getVoteCollection, getProposalCollection } = require("../../mongo");
 const { normalizeAddress } = require("../../utils/address");
 const { calcWeights } = require("../../services/proposal.service/common");
-const { getAnonymousVote } = require("../../utils/anonymous");
 
 async function getUserVotesOfProposals(ctx) {
   const { network, address, proposal_cid: proposalCid } = ctx.query;
@@ -26,12 +25,12 @@ async function getUserVotesOfProposals(ctx) {
   const proposals = await proposalCol
     .find({ cid: { $in: proposalCids } })
     .toArray();
-  const anonymousProposals = new Set(
-    proposals.filter((p) => p.anonymous).map((p) => p.cid),
+  const nonAnonymousProposals = new Set(
+    proposals.filter((p) => !p.anonymous).map((p) => p.cid),
   );
 
   const q = {
-    "data.proposalCid": { $in: proposalCids },
+    "data.proposalCid": { $in: nonAnonymousProposals },
     voter: { $in: [address, normalizedAddress] },
     voterNetwork: network,
   };
@@ -39,13 +38,7 @@ async function getUserVotesOfProposals(ctx) {
   const voteCol = await getVoteCollection();
   const votes = await voteCol.find(q).toArray();
 
-  ctx.body = votes.map((vote) => {
-    const v = calcWeights(vote);
-    if (anonymousProposals.has(vote.data.proposalCid)) {
-      return getAnonymousVote(v);
-    }
-    return v;
-  });
+  ctx.body = votes.map((vote) => calcWeights(vote));
 }
 
 module.exports = {
