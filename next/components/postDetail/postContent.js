27c38d9ae6d3d2327050de6bb1db650b65a9001d
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-10-31 17:10:37 +0800

    Add scroll-based title display on post content, #648 (#1242)
    * feat: add scroll-based title display in post content, #648
    
    * fix: adjust title positioning on scroll, #648

--- next/components/postDetail/postContent.js
+++ next/components/postDetail/postContent.js
@@ -18,6 +18,10 @@ import { MarkdownPreviewer } from "@osn/previewer";
 import PostBanner from "@/components/postDetail/postBanner";
 import { isSameAddress } from "frontedUtils/address";
 import { isCollectiveSpace } from "frontedUtils/space";
+import { ArrowCaretLeft } from "@osn/icons/opensquare";
+import { useCallback, useEffect, useRef, useState } from "react";
+import debounce from "lodash.debounce";
+import { cn } from "@osn/common-ui";
 
 const Title = styled.div`
   ${p_semibold};
@@ -79,7 +83,7 @@ export default function PostContent({ data, space, loadSuggestions }) {
 
   return (
     <Panel>
-      <Title>{data?.title}</Title>
+      <TitleContent title={data?.title} />
       <InfoWrapper>
         <LeftWrapper>
           <Author
@@ -123,3 +127,53 @@ export default function PostContent({ data, space, loadSuggestions }) {
     </Panel>
   );
 }
+
+const TitleContent = ({ title }) => {
+  const titleRef = useRef();
+  const [offsetTop, setOffsetTop] = useState(152);
+  const [scrollY, setScrollY] = useState(0);
+  const debouncedScrollHandler = useRef(null);
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY);
+  }, []);
+
+  useEffect(() => {}, [handleScroll]);
+
+  useEffect(() => {
+    if (!titleRef.current) {
+      return;
+    }
+    const { top, height } = titleRef.current.getBoundingClientRect();
+    setOffsetTop(top + height - 66);
+    debouncedScrollHandler.current = debounce(handleScroll, 10);
+    window.addEventListener("scroll", debouncedScrollHandler.current);
+
+    return () => {
+      if (debouncedScrollHandler.current) {
+        debouncedScrollHandler.current.cancel();
+        window.removeEventListener("scroll", debouncedScrollHandler.current);
+      }
+    };
+  }, [handleScroll]);
+
+  return (
+    <>
+      <Title ref={titleRef}>{title}</Title>
+      <div
+        className={cn(
+          "fixed py-5  z-10 top-0 transform -translate-y-full left-0 w-full bg-fillBgPrimary border border-strokeBorderDefault shadow-shadowCardDefault",
+          scrollY > offsetTop && "translate-y-0",
+        )}
+      >
+        <div className="md:max-w-[1144px] mx-auto md:px-8 px-5 flex relative">
+          <div className="w-full md:!w-[calc(100%-320px)] flex items-center">
+            <ArrowCaretLeft className="w-8 h-8 p-1 md:ml-8 ml-5 rounded-full border shadow  absolute left-0 -translate-x-1/2 " />
+            <div className="pl-8 md:px-8 font-[700] text-base md:font-[600] md:text-[20px] whitespace-nowrap overflow-hidden text-ellipsis">
+              {title}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
