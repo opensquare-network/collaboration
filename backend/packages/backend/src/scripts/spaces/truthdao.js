87d23d2a9e2dc4d7a234e9b3f511c97710fc6182
chaojun <chaojun@opensquare.network>
2026-03-05 17:42:07 +0800

    Remove debug data

--- backend/packages/backend/src/scripts/spaces/truthdao.js
+++ backend/packages/backend/src/scripts/spaces/truthdao.js
@@ -14,7 +14,6 @@ const config = {
     "16ap6fdqS2rqFsyYah35hX1FH6rPNWtLqqXZDQC9x6GW141C", // Lurpis
     "14pa3BAYZLPvZfRDjWEfZXZWBVU45E67HUQEUxNCrdXGoata", // Tiny/Mimir
     "14qwyVVvW4Tuhq4Fvt2AHZqhbCtGfVb8HUY2xM2PKrzKsmZT",
-    "12ZBQqk7SD9qA7qPwpzAND6ZznXtgZuCMFWZr8xpdsVdbeva",
   ],
   weightStrategy: [strategies.onePersonOneVote],
   version: "4",
@@ -23,7 +22,6 @@ const config = {
   admins: [
     "15MFgTrCqjnLRPkDymuuhBMpzxQmcRyfFDETRiEtBRjpfLzE",
     "14gMJV95zwxUsFEZDSC8mtBVifS6SypKJkfBKANkMsLZdeVb", // Yongfeng
-    "12ZBQqk7SD9qA7qPwpzAND6ZznXtgZuCMFWZr8xpdsVdbeva",
   ],
 };
 
